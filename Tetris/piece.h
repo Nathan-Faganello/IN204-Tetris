3fa8f4d0bd6f18c42da7eada897d96ec7cde6b83
aulaforge <aurelien.laforge@ensta-paris.fr>
2021-01-05 11:56:37 +0100

    update piece.h, notamment classe

--- Tetris/piece.h
+++ Tetris/piece.h
@@ -203,12 +203,51 @@ static const int pieces[NB_PIECE][NB_ROTA][TAILLE_MAT][TAILLE_MAT] = {
 
 }
 
+
+enum class Type = { I, O, T, L, J, Z, S };
+enum class Couleur = { CYAN, JAUNE, ORANGE, VIOLET, BLEU, ROUGE, VERT };
+
 class Piece {
-	private
+private:
+	Type type;
+	int rotation;
+	int position_X;
+	int position_Y;
+	Couleur couleur;
 
-
-
-
+public:
+	Piece();
+	Piece(Type theType, int theRota, int theX, int theY, Couleur theColor): type(theType), rotation(theRota), position_X(theX), position_Y(theY), couleur(theColor) {}
+	void setType(Type theType) {
+		type = theType;
+	}
+	void setRota(int theRota) {
+		rotation = theRota;
+	}
+	void setCouleur(Couleur theColor) {
+		couleur = theColor;
+	}
+	void setPosX(int theX) {
+		position_X = theX;
+	}
+	void setPoxY(int theY) {
+		position_Y = theY;
+	}
+	Type getType() {
+		return type;
+	}
+	int getRota() {
+		return rotation;
+	}
+	int getPosX() {
+		return position_X;
+	}
+	int getPosY() {
+		return position_Y;
+	}
+	Couleur getCouleur() {
+		return couleur;
+	}
 };
 
 #endif // !PIECE_H
