535f44e2fd9ce83e01b8708700dd6350aa6b6c39
aulaforge <aurelien.laforge@ensta-paris.fr>
2021-02-07 17:52:09 +0100

    update calculScore

--- Tetris/game.h
+++ Tetris/game.h
@@ -3,7 +3,29 @@
 
 
 
-int calculScore(int score, int niveau, int gainNiv0, int nbLigne);
+int calculScore(int score, int niveau, int nbLignes){
+  int ajout;
+  switch(nbLignes)
+  {
+    case 1:
+      ajout=40;
+      break;
+    case 2:
+      ajout=100;
+      break;
+    case 3:
+      ajout=300;
+      break;
+    case 4:
+      ajout=1200;
+      break;
+    default:
+      std::cerr<<"Wrong number of lines destructed"<<std::enl;
+      ajout=0;
+      break;
+  }
+  return score+(niveau+1)*ajout;
+}
 
 
 
