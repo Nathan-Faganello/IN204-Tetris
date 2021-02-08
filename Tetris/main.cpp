5f0cf6d603e23fb624bd3425a96865603aa29e84
aulaforge <aurelien.laforge@ensta-paris.fr>
2021-02-08 17:22:21 +0100

    affichage score + aire de jeu (test, à implémenter dans une fonction)

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -10,10 +10,11 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "Tetris Faganello Laforge");
 		window.setFramerateLimit(30);
 		int niveau=0;
 		int statut=0;
+    int score=0;
 
     while (window.isOpen())
     {
@@ -32,6 +33,9 @@ int main()
 									if (niveau==0){niveau=16;}
 									niveau=niveau-1;
 								}
+                else if (event.key.code == sf::Keyboard::Enter){
+                  statut++;
+                }
 							}
 						}
 
@@ -43,7 +47,7 @@ int main()
     			std::cout << "Error loading font\n" ;
 				}
 
-				if (statut==0){
+				if (statut==0){ //affichage texte bienvenue
 					sf::Text text;
 					text.setString("Bienvenue dans notre Tetris");
 					text.setFont(font);
@@ -55,7 +59,7 @@ int main()
 					text.setPosition(sf::Vector2f(1080/2.0f,720/2.0f));
 					window.draw(text);
 					window.display();
-					sf::Time t1 = sf::seconds(2.0);
+					sf::Time t1 = sf::seconds(1.0);
 					sf::sleep(t1);
 
 
@@ -63,7 +67,7 @@ int main()
 
 				}
 
-				if(statut==1){
+				if(statut==1){  //choix du niveau
 					window.clear();
 					sf::Text affNiveau;
 					affNiveau.setString("Niveau");
@@ -102,6 +106,111 @@ int main()
 					window.draw(valNiveau);
 					window.display();
 			}
+
+      if(statut==2){
+        window.clear();
+
+        //affichage cadre de jeu
+
+        sf::Vertex lineVert1[] =
+        {
+            sf::Vertex(sf::Vector2f(100, 30)),
+            sf::Vertex(sf::Vector2f(100, 690))
+        };
+
+        window.draw(lineVert1, 2, sf::Lines);
+
+        sf::Vertex lineVert2[] =
+        {
+            sf::Vertex(sf::Vector2f(400, 30)),
+            sf::Vertex(sf::Vector2f(400, 690))
+        };
+
+        window.draw(lineVert2, 2, sf::Lines);
+
+        sf::Vertex lineHori1[] =
+        {
+            sf::Vertex(sf::Vector2f(100, 30)),
+            sf::Vertex(sf::Vector2f(400, 30))
+        };
+
+        window.draw(lineHori1, 2, sf::Lines);
+
+        sf::Vertex lineHori2[] =
+        {
+            sf::Vertex(sf::Vector2f(100, 690)),
+            sf::Vertex(sf::Vector2f(400, 690))
+        };
+
+        window.draw(lineHori2, 2, sf::Lines);
+
+        //affichage score
+
+
+
+        sf::Vertex scLineVert1[] =
+        {
+            sf::Vertex(sf::Vector2f(500, 300)),
+            sf::Vertex(sf::Vector2f(500, 400))
+        };
+
+        window.draw(scLineVert1, 2, sf::Lines);
+
+        sf::Vertex scLineVert2[] =
+        {
+            sf::Vertex(sf::Vector2f(800, 300)),
+            sf::Vertex(sf::Vector2f(800, 400))
+        };
+
+        window.draw(scLineVert2, 2, sf::Lines);
+
+        sf::Vertex scLineHori1[] =
+        {
+            sf::Vertex(sf::Vector2f(500, 300)),
+            sf::Vertex(sf::Vector2f(800, 300))
+        };
+
+        window.draw(scLineHori1, 2, sf::Lines);
+
+        sf::Vertex scLineHori2[] =
+        {
+            sf::Vertex(sf::Vector2f(500, 400)),
+            sf::Vertex(sf::Vector2f(800, 400))
+        };
+
+        window.draw(scLineHori2, 2, sf::Lines);
+
+        sf::Text txtScore;
+        txtScore.setString("Score :");
+        txtScore.setFont(font);
+        txtScore.setCharacterSize(30);
+        txtScore.setFillColor(sf::Color::White);
+
+        sf::FloatRect textRect = txtScore.getLocalBounds();
+        txtScore.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+        txtScore.setPosition(sf::Vector2f(650,320));
+        window.draw(txtScore);
+
+
+        std::string txtValScore=std::to_string(score);
+
+        sf::Text valScore;
+        valScore.setString(txtValScore);
+        valScore.setFont(font);
+        valScore.setCharacterSize(30);
+        valScore.setFillColor(sf::Color::White);
+
+        textRect = valScore.getLocalBounds();
+        valScore.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+        valScore.setPosition(sf::Vector2f(650,370));
+        window.draw(valScore);
+
+
+
+        window.display();
+
+
+      }
 
 
     }
